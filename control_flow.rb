648e137d1cad45c8141b55117f866ef75553d30a
Kelsey <kdoyle390@gmail.com>
2021-11-27 18:03:05 -0600

    commit tree 08737ae1c0924a7780958ce457e99fcf1754bbcb

--- control_flow.rb
+++ control_flow.rb
@@ -1,8 +1,21 @@
 def admin_login(username, password)
-  # your code here
+  if username == "admin"
+      puts "Access granted"
+    else
+      puts "Access denied"
+    end 
 end
 
 def hows_the_weather(temperature)
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature > 40 && temperature < 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
   # your code here
 end
 
